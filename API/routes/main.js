414cfe5fdfb1cddb8211b9434ac227ac51ee8320
Christian <chsanleo@gmail.com>
2020-06-14 01:26:28 +0200

    Installation swagger
    npm install swagger-ui-express
    npm install swagger-jsdoc --save

--- API/routes/main.js
+++ API/routes/main.js
@@ -1,6 +1,12 @@
 const router = require('express').Router();
 const MainController = require('../controllers/MainController');
 
+/**
+ * @swagger
+ * /main:
+ *    get:
+ *      description: This should return if is alive or not
+ */
 router.get('/', MainController.isAlive);
 
 module.exports = router;
